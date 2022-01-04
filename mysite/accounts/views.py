5310658372551bede29c0eeeded1309d96c1fd08
Koshin Obayashi <bayamax@icloud.com>
2022-01-05 00:12:09 +0900

    Revert "あ"
    This reverts commit 0c8f48800cd1cf55dcc991bcbf242b553de46152.

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,8 +1,3 @@
-from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
-def user_profile(request, username):
-    context = {
-        'User': get_user_model().objects.get(username=username),
-    }
-    return render(request, 'accounts/user_profile.html', context)
+# Create your views here.
